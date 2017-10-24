1f2f5d0d21027393b99f7cefed8af4a202a1d836
Min Khati <meetup.min@gmail.com>
2017-10-24 10:51:14 -0400

    Add user validation, user email idex and test

---
+++ test/models/user_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "example@email.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "A" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "E" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email should be valid email address" do
+    valid_email = %w[user@example.com USER@foo.COM A_US_eR@foo.bar.org first.last@foo.jp alice+bob@bab.cn]
+    valid_email.each do |email|
+      @user.email = email
+      assert @user.valid?, "Email address #{email.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid email address" do
+    invalid_email = %w[user@example,com USER_at_foo.COM A_US_eR#foo.bar.org first.last.foo.jp alice+bob@bab_baz.cn example.ex@example.10]
+    invalid_email.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+end
