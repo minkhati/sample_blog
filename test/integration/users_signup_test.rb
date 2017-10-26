5283f11b39d0f33926a676d1025dee6e9b7af719
Min Khati <meetup.min@gmail.com>
2017-10-26 11:37:48 -0400

    Implement basic login/logout with testcase

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -43,6 +43,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.nil?
     assert_not flash.empty?
   end
