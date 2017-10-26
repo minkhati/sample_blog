fdd96af6c5f28add3a229f440e8c9641967c0ac3
Min Khati <meetup.min@gmail.com>
2017-10-26 16:37:44 -0400

    finish log/log out with remember me option

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,8 +50,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "password should have a minimum length" do
-    @user.email = @user.password_confirmation = "E" * 5
+    @user.email = @user.password_confirmation = "E" * 8
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 
 end
