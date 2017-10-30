5da5841b908afb21b4854528d818564878f051b8
Min Khati <meetup.min@gmail.com>
2017-10-30 15:03:16 -0400

    Add account activation, send account activation email

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,27 +6,18 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
+  test "index as admin including pagination and delete links" do
     log_in_as(@admin)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
-  end
-
-  test "index as ad admin including pagination and delete links" do
-    log_in_as(@admin)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text:   user.name
+    first_page_of_users.first.toggle!(:activated)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    assigns(:users).each do |user|
+      assert user.activated?
+      assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text:   'delete',
-                                                  method: :delete
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
     assert_difference 'User.count', -1 do
