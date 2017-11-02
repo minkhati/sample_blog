827f8654977d825b87bfa536a9c32ab16299e546
Min Khati <meetup.min@gmail.com>
2017-11-02 16:25:12 -0400

    Add user micropost and image file upload using carrier wave, image magic, fog (production) and S3 bucket configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -69,6 +71,11 @@ class User < ApplicationRecord
     reset_sent_at <  2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "following users" for the full implementations.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
