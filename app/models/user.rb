fde015b9eaab47973acfd69d1e0538b712dece8b
Min Khati <meetup.min@gmail.com>
2017-11-03 18:41:29 -0400

    Add user following and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -71,10 +77,26 @@ class User < ApplicationRecord
     reset_sent_at <  2.hours.ago
   end
 
-  # Defines a proto-feed.
-  # See "following users" for the full implementations.
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids_subselect = "SELECT followed_id FROM relationships
+                               WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids_subselect}) OR user_id = :user_id", user_id: id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
